1e3edb76b9be945801ca9a173c99cdf04cb8ca76
Abhay Ratnakar <a.s.ratnakar2192@gmail.com>
2024-12-29 00:48:17 +0530

    Register functionality

--- Backend/app.js
+++ Backend/app.js
@@ -4,12 +4,19 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 connectToDb();
 
 app.get('/', (req, res)=>{
     res.send("Hello world");
 })
+
+app.use('/users', userRoutes);
 
 module.exports = app;
